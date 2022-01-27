8290b3ee3a8ac8e35b3f36bf396023c9fc5f291a
natalieycyoung <44858512+natalieycyoung@users.noreply.github.com>
2022-01-26 16:26:08 -0800

    Got MarkdownParseTest.java working

---
+++ MarkdownParseTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
+
+public class MarkdownParseTest
+{
+    @Test
+    public void addition()
+	{
+        assertEquals(2, 1 + 1);
+    }
+
+	@Test
+	public void multiplication()
+	{
+		assertEquals(4, 2 * 2);
+	}
+
+	@Test
+	public void testGetLinks() throws IOException
+	{
+		Path fileName = Path.of("test-file.md");
+		String contents = Files.readString(fileName);
+		
+		assertEquals("[https://something.com, some-page.html]", MarkdownParse.getLinks(contents).toString());
+	}
+}
