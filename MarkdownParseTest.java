aba1d3470150456ee7a47ce46384a3784e790f73
natalieycyoung <44858512+natalieycyoung@users.noreply.github.com>
2022-01-26 16:56:24 -0800

    Need to Debug Since testFile3 Induced Failure

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -26,4 +26,13 @@ public class MarkdownParseTest
 		
 		assertEquals("[https://something.com, some-page.html]", MarkdownParse.getLinks(contents).toString());
 	}
+
+	@Test
+	public void testGetLinksFile3() throws IOException
+	{
+		Path fileName = Path.of("testFile3.md");
+		String contents = Files.readString(fileName);
+		
+		assertEquals("[https::look parentheses()]", MarkdownParse.getLinks(contents).toString());
+	}
 }
